be4adf43e8c971ef198c0f33ea8f578a475cbc3d
Fardale <fardale+git@crans.org>
2020-12-16 23:24:53 +0100

    test: add test for sourceLanguage

--- test/test.ml
+++ test/test.ml
@@ -54,7 +54,7 @@ module T = struct
                          (self (n / 2)) );
                      ( 2,
                        int_range 0 20 >>= fun i ->
-                       map nprod (list_size (return i) (self (n / max 1 i))) );
+                       map nprod (list_repeat i (self (n / max 2 i))) );
                      (1, return Real);
                    ]))
 
@@ -416,6 +416,196 @@ module T = struct
       opti_list
 end
 
+module S = struct
+  open SourceLanguage
+
+  let real = Real
+
+  let prod x y = Prod (x, y)
+
+  let type_gen depth =
+    QCheck.Gen.(
+      sized_size (int_bound depth)
+      @@ fix (fun _self n ->
+             match n with
+             | 0 -> return Real
+             | _n ->
+                 frequency
+                   [
+                     (*(2, map2 prod (self (n / 2)) (self (n / 2)));*)
+                     (1, return Real);
+                   ]))
+
+  let var x t = Var (x, t)
+
+  let const x = Const x
+
+  let apply1 op exp = Apply1 (op, exp)
+
+  let apply2 op exp1 exp2 = Apply2 (op, exp1, exp2)
+
+  let clet v t exp1 exp2 = Let (v, t, exp1, exp2)
+
+  let find_by_ty ty = List.find_opt (fun (_, t) -> equalTypes t ty)
+
+  let dist_to_type (targetTy : sourceType) (ty : sourceType) =
+    match (targetTy, ty) with
+    | Real, Real -> Some 0
+    | _ -> None
+
+  let random_closest_type targetTy tyList =
+    let open QCheck.Gen in
+    List.mapi
+      (fun i ty ->
+        match dist_to_type targetTy ty with
+        | None -> None
+        | Some d -> Some (ty, i, d))
+      tyList
+    |> CCList.keep_some
+    |> function
+    | [] -> None
+    | (_, _, d) :: tl as l ->
+        let d_min = List.fold_left (fun x (_, _, d) -> min x d) d tl in
+        Some (generate1 (oneofl (List.filter (fun (_, _, d) -> d = d_min) l)))
+
+  let rec complet_to_type _context n (targetTy : sourceType) (term : sourceSyn)
+      (ty : sourceType) =
+    if equalTypes targetTy ty then Some term
+    else if n <= 0 then None
+    else
+      match (targetTy, ty) with
+      | Real, Real -> Some term
+      | _ -> None
+
+  and get_from_context context n targetTy =
+    List.filter_map
+      (fun (v, t) ->
+        match dist_to_type targetTy t with
+        | None -> None
+        | Some d -> Some (v, t, d))
+      context
+    |> function
+    | [] -> None
+    | (_, _, d) :: t as l -> (
+        let d_min = List.fold_left (fun x (_, _, d) -> min x d) d t in
+        match List.filter (fun (_, _, d) -> d = d_min) l with
+        | [] -> None
+        | l ->
+            let v, t, _ = QCheck.Gen.(generate1 (oneofl l)) in
+            complet_to_type context n targetTy (Var (v, t)) t)
+
+  and term_gen context (n : int) targetTy =
+    QCheck.Gen.(
+      sized_size (return n) @@ fun i ->
+      fix
+        (fun self (n, context, targetTy) ->
+          if n <= 0 then
+            match targetTy with
+            | Real -> map const (float_bound_exclusive 1.)
+            | Prod (_x1,_x2) -> failwith "Prod: not implemented"
+          else
+            let let_gen =
+              let newVar = Vars.fresh () in
+              let varType = generate1 (type_gen (n / 10)) in
+              let newContext = (newVar, varType) :: context in
+              map2
+                (fun expr1 expr2 -> clet newVar varType expr1 expr2)
+                (self (n / 4, context, varType))
+                (self (3 * n / 4, newContext, targetTy))
+            in
+            let gen_list = [ let_gen ] in
+            let gen_list =
+              match get_from_context context n targetTy with
+              | None -> gen_list
+              | Some term -> return term :: gen_list
+            in
+            match targetTy with
+            | Real ->
+                oneof
+                  (map2 apply1 Op.gen1 (self (n - 1, context, Real))
+                  :: map3 apply2 Op.gen2
+                       (self (n / 2, context, Real))
+                       (self (n / 2, context, Real))
+                  :: map const (float_bound_exclusive 1.)
+                  :: gen_list)
+            | _ -> oneof gen_list)
+        (i, context, targetTy))
+
+  let rec shrink_term expr =
+    let open QCheck.Iter in
+    match expr with
+    | Var (_, _) | Const _ -> empty
+    | Apply1 (op, expr) ->
+        return expr
+        <+> (shrink_term expr >|= apply1 op)
+        <+> (Op.shrink_op1 op >|= fun op -> apply1 op expr)
+    | Apply2 (op, expr1, expr2) ->
+        of_list [ expr1; expr2 ]
+        <+> (shrink_term expr1 >|= fun expr -> apply2 op expr expr2)
+        <+> (shrink_term expr2 >|= fun expr -> apply2 op expr1 expr)
+    | Let (x, t, expr1, expr2) ->
+        shrink_term expr1
+        >|= (fun expr -> clet x t expr expr2)
+        <+> (shrink_term expr2 >|= fun expr -> clet x t expr1 expr)
+
+  let arbitrary_closed_term =
+    QCheck.make
+      QCheck.Gen.(int_bound 20 >>= fun i -> term_gen [] i Real)
+      ~print:to_string ~shrink:shrink_term
+
+  let arbitrary_term =
+    QCheck.make
+      QCheck.Gen.(
+        int_bound 20 >>= fun i ->
+        term_gen (List.init 10 (fun i -> (("x", i), Real))) i Real)
+      ~print:to_string ~shrink:shrink_term
+
+  let test_isWellTyped =
+    QCheck.Test.make ~count:1000 ~name:"isWellTyped" arbitrary_closed_term
+      isWellTyped
+
+  let test_equalTerms =
+    QCheck.Test.make ~count:1000 ~name:"equalTerms" arbitrary_closed_term
+      (fun expr -> equalTerms expr expr)
+
+  let test_interpret =
+    QCheck.Test.make ~count:1000 ~name:"interp" arbitrary_closed_term
+      (fun expr -> ignore(interpret expr []); true)
+
+  let test_anf =
+    QCheck.Test.make ~count:1000 ~name:"anf" arbitrary_closed_term (fun expr ->
+        Float.equal
+          (interpret (Transforms.Anf.SourceAnf.anf expr) [])
+          (interpret expr []))
+
+  let test_weakAnf =
+    QCheck.Test.make ~count:1000 ~name:"weakAnf" arbitrary_closed_term
+      (fun expr ->
+        Float.equal
+          (interpret (Transforms.Anf.SourceAnf.weakAnf expr) [])
+          (interpret expr []))
+
+  let test_isInAnf_anf =
+    QCheck.Test.make ~count:1000 ~name:"isInAnf.anf" arbitrary_closed_term
+      (fun expr -> Transforms.Anf.SourceAnf.(isInAnf (anf expr)))
+
+  let test_isInWeakAnf_weakAnf =
+    QCheck.Test.make ~count:1000 ~name:"isInWeakAnf.weakAnf"
+      arbitrary_closed_term (fun expr ->
+        Transforms.Anf.SourceAnf.(isInWeakAnf (weakAnf expr)))
+
+  let test_list =
+    [
+      test_isWellTyped;
+      test_equalTerms;
+      test_interpret;
+      test_anf;
+      test_weakAnf;
+      test_isInWeakAnf_weakAnf;
+    ]
+
+end
+
 let () =
   Format.printf "%a@." TargetLanguage.pp
     (QCheck.Gen.generate1 (T.term_gen [] 20 TargetLanguage.Real))
@@ -426,9 +616,11 @@ let () =
   let target_opti_freeVar =
     List.map QCheck_alcotest.to_alcotest T.test_opti_freeVar_list
   in
+  let source = List.map QCheck_alcotest.to_alcotest S.test_list in
   Alcotest.run "Main test"
     [
       ("Target Language", target);
       ("Opti Target Language", target_opti);
       ("Opti Free Vars Target Language", target_opti_freeVar);
+      ("Source Language", source);
     ]
