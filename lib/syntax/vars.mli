f7d805cd52e498b93bbc3708e523d405d8d7f4e9
Fardale <fardale+git@crans.org>
2020-12-21 12:12:59 +0100

    doc: add some doc for Vars and Operators

--- lib/syntax/vars.mli
+++ lib/syntax/vars.mli
@@ -1,5 +1,16 @@
+(** Module for the variable of the target and source language *)
+
 type t = string * int
+(** A variable is a name and a numbre, for example  *)
+
 val fresh : unit -> t
+(** [fresh ()] create a fresh variable assuming that the name ["y"] is not used. *)
+
 val equal : t -> t -> bool
-val to_string : t -> string
+(** Equality between variables *)
+
 val pp : Format.formatter -> t -> unit
+(** Pretty printer *)
+
+val to_string : t -> string
+(** [to_string v] print [v] in a string using {!pp} *)
