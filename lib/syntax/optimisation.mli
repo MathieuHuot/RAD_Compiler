7d878fd7c0d1a667b1e78b91972daffa5e0165e7
Mathieu <mathieu.huot@stx.ox.ac.uk>
2021-01-31 20:38:59 +0100

    simple .mli for opti

--- lib/syntax/optimisation.mli
+++ lib/syntax/optimisation.mli
@@ -1,1 +1,12 @@
-TODO
+val constantPropagation : Target.t -> Target.t Rewriter.output
+  val simpleAlgebraicSimplifications : Target.t -> Target.t Rewriter.output
+  val zeroSimplification : Target.t -> Target.t Rewriter.output
+  val trigoSimplification : Target.t -> Target.t Rewriter.output
+  val realFactorisation : Target.t -> Target.t Rewriter.output
+  val letCommutativity : Target.t -> Target.t Rewriter.output
+  val forwardSubstitution : Target.t -> Target.t Rewriter.output
+  val letSimplification : Target.t -> Target.t Rewriter.output
+  val lambdaRemoval : Target.t -> Target.t Rewriter.output
+  val deadVarElim : Target.t -> Target.t Rewriter.output
+  val oneCaseRemoval : Target.t -> Target.t Rewriter.output
+  val fullOpti : Target.t -> Target.t
