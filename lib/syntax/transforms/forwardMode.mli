fca438b56c0aa549f152935bd1e94e6adb59ab9b
Mathieu <mathieu.huot@stx.ox.ac.uk>
2021-02-02 11:12:42 +0100

    minor forward mode

--- lib/syntax/transforms/forwardMode.mli
+++ lib/syntax/transforms/forwardMode.mli
@@ -2,4 +2,4 @@ type gradient_variables = (Syntax.Var.t * Syntax.Source.Type.t) Syntax.Source.tu
 
 val forwardADType : Syntax.Source.Type.t -> Syntax.Target.Type.t
 val forwardAD : Syntax.Source.t -> Syntax.Target.t
-val grad : gradient_variables -> Syntax.Source.t -> Syntax.Target.t list
+val grad : gradient_variables -> Syntax.Source.t -> Syntax.Target.t
